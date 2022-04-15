0bdfc73277068426ac10a3f13c76b355f9676c77
tejas <tejasshah@gmail.com>
2022-04-15 18:49:27 +0530

    first servlet

---
+++ src/com/controller/SignupServlet.java
@@ -0,0 +1,17 @@
+package com.controller;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+//HttpServlet --> servlet-api.jar 
+public class SignupServlet extends HttpServlet {
+
+	// init
+	// service
+	// destroy
+
+	public void service(HttpServletRequest request, HttpServletResponse response) {
+
+	}
+}
