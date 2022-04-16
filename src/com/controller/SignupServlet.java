2ebc042c6be87d236aeb1411f87e479bed3313a4
tejas <tejasshah@gmail.com>
2022-04-16 17:46:21 +0530

    servlet

--- src/com/controller/SignupServlet.java
+++ src/com/controller/SignupServlet.java
@@ -1,5 +1,7 @@
 package com.controller;
 
+import java.io.PrintWriter;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,6 +14,27 @@ public class SignupServlet extends HttpServlet {
 	// destroy
 
 	public void service(HttpServletRequest request, HttpServletResponse response) {
+		String firstName = request.getParameter("firstName");
+		String email = request.getParameter("email");
+		String password = request.getParameter("password");
+
+		System.out.println(firstName);
+		System.out.println(email);
+		System.out.println(password);
+
+		response.setContentType("text/html");
+
+		try {
+			PrintWriter out = response.getWriter();
+			out.print("<html><body>");
+			out.print("FirstName : " + firstName + "<br>");
+			out.print("Email : " + email + "<br>");
+			out.print("Password : " + password);
+			out.print("</body></html>");
+
+		} catch (Exception e) {
+
+		}
 
 	}
 }
