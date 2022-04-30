3ac980e83cacdcbc51b05d2221c0f4f229b4b1c4
tejas <tejasshah@gmail.com>
2022-04-30 18:52:27 +0530

    insert

--- src/com/controller/RegistrationServlet.java
+++ src/com/controller/RegistrationServlet.java
@@ -2,12 +2,19 @@ package com.controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.bean.UserBean;
+import com.dao.UserDao;
+import com.mysql.cj.xdevapi.PreparableStatement;
+import com.util.DbConnection;
 
 public class RegistrationServlet extends HttpServlet {
 
@@ -46,6 +53,16 @@ public class RegistrationServlet extends HttpServlet {
 			RequestDispatcher rd = request.getRequestDispatcher("Registration.jsp");
 			rd.forward(request, response);
 		} else {
+
+			// db insert
+			UserBean user = new UserBean();
+			user.setFirstName(firstName);
+			user.setEmail(email);
+			user.setPassword(password);
+			
+			
+			
+			new UserDao().insertUser(user);
 
 			RequestDispatcher rd = request.getRequestDispatcher("Home.jsp");
 			rd.forward(request, response);
--- src/com/util/DbConnection.java
+++ src/com/util/DbConnection.java
@@ -2,10 +2,11 @@ package com.util;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 
 public class DbConnection {
 
-	public static void main(String[] args) {
+	public static Connection getConnection() {
 
 		String driver = "com.mysql.cj.jdbc.Driver";
 		String url = "jdbc:mysql://localhost:3306/advjavaavengers";
@@ -15,15 +16,18 @@ public class DbConnection {
 		try {
 			Class.forName(driver);
 			Connection con = DriverManager.getConnection(url, userName, password);
-			if(con == null) {
-				System.out.println("dBNotConnected..");
-			}else {
-				System.out.println("dbConnected...");
-			}
-			
+			// users -- userid firstname email password
+
+			// insert into users (firstname,email,password) values ('a','a','a');
+			// Statement
+			// PreparedStatement
+			// CallableStatement***
+
+			System.out.println("done...");
+			return con;
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-
+		return null;
 	}
 }
